020d6cc5c30b9133c60f3d9e914a8e8df22a6cf2
ajsilva1119 <89565246+ajsilva1119@users.noreply.github.com>
2022-02-09 12:49:18 -0700

    Add files via upload

---
+++ fa/dfa/DFADriver.java
@@ -0,0 +1,89 @@
+package fa.dfa;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * January 27, 2022
+ * The class reads the input file and instantiates a DFA from it.
+ * Next it reads a string from the same file and prints "yes" if the string 
+ * is in the DFA's language or "not if the string is not in the DFA's language.
+ * Also returns whether the swapped copy is correct.
+ * @author elenasherman
+ *
+ */
+public class DFADriver {
+
+	/**
+	 * @param args - a file name containing a DFA encoding and a set of strings.
+	 * @throws FileNotFoundException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		//The file name is passed as an argument
+		String fileName = args[0];
+		File file = new File(fileName);
+		//System.out.println(file.exists());
+		if(file.exists()){
+			//create a DFA instance -- you need to write DFA.java that
+			//implements DFAInterface.java
+			DFA dfa = new DFA();
+			Scanner scan = new Scanner(file);
+			//the first line is the set of final states
+			//get the string of the final states and split it on a space
+			String finalStates = scan.nextLine().trim();
+			//using tokenizer to split the string
+			StringTokenizer tk = new StringTokenizer(finalStates, " ");
+			while(tk.hasMoreTokens()){
+				dfa.addFinalState(tk.nextToken());
+			}
+			
+			//the second line is the start state
+			String startStateName = scan.nextLine().trim();
+			dfa.addStartState(startStateName);
+
+			//get the string of other states and split in on space too
+			String otherStates = scan.nextLine().trim();
+			tk = new StringTokenizer(otherStates, " ");
+			while(tk.hasMoreTokens()){
+				dfa.addState(tk.nextToken());
+			}
+
+			//read in the transactions
+			String trans = scan.nextLine();
+			tk = new StringTokenizer(trans, " ");
+			while(tk.hasMoreTokens()){
+				char[] tran = tk.nextToken().toCharArray();
+				dfa.addTransition(String.valueOf(tran[0]), tran[1], String.valueOf(tran[2]));
+			}
+
+			//print out dfa in the specified format.
+			System.out.println(dfa.toString());
+			//data for swapping
+			Iterator<Character> abcIter = dfa.getABC().iterator();
+			char symb1 = abcIter.next();
+			char symb2 = abcIter.next();
+			DFA dfaSW = dfa.swap(symb1, symb2);
+			boolean correctSW = true;
+			
+			//now process the input strings
+			while(scan.hasNext()){
+					String input = scan.nextLine();
+					boolean accepts = dfa.accepts(input);
+					System.out.println(accepts?"yes":"no");
+					String inputSW = input.replace(symb1, '-').replace(symb2, symb1).replace('-', symb2);
+					boolean acceptsSW = dfaSW.accepts(inputSW);
+					if(correctSW) {
+						correctSW = accepts == acceptsSW;
+					}
+			}
+			System.out.println("\nIs swapped DFA correct? : " + (correctSW?"yes":"no"));
+			scan.close();
+		} else {
+			System.err.println(file + " does not exists - please check the file path");
+		}
+	}
+
+}
---
+++ fa/dfa/DFAInterface.java
@@ -0,0 +1,52 @@
+package fa.dfa;
+
+import fa.FAInterface;
+import fa.State;
+
+
+public interface DFAInterface extends FAInterface{
+
+	/**
+	 * Construct the textual representation of the DFA, for example
+	 * A simple two state DFA
+	 * Q = { a b }
+	 * Sigma = { 0 1 }
+	 * delta =
+	 *		0	1	
+	 *	a	a	b	
+	 *	b	a	b	
+	 * q0 = a
+	 * F = { b }
+	 * 
+	 * The order of the states and the alphabet is the order
+	 * in which they were instantiated in the DFA.
+	 * @return String representation of the DFA
+	 */
+	public abstract String toString();
+	
+	/**
+	 * Simulates a DFA on input s to determine
+	 * whether the DFA accepts s.
+	 * @param s - the input string
+	 * @return true if s in the language of the DFA and false otherwise
+	 */
+	public abstract boolean accepts(String s);
+	
+	/**
+	 * Uses transition function delta of FA
+	 * @param from the source state
+	 * @param onSymb the label of the transition
+	 * @return the sink state.
+	 */
+	public abstract State getToState(DFAState from, char onSymb);
+	
+	/**
+	 * Creates a deep copy of this DFA
+	 * which transitions labels are
+	 * swapped between symbols symb1
+	 * and symb2.
+	 * @return a copy of this DFA
+	 */
+	public DFA swap(char symb1, char symb2);
+
+}
